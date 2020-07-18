7cdb91c381ac52df805595a021f9b1cd91e68955
HADES-01 <adityavats5338@gmail.com>
2020-07-18 16:26:08 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+let express = require("express");
+let request = require("request");
+let ejs = require("ejs");
+let app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/result/:no", function (req, res) {
+    let query = req.query.Search;
+    let page = req.params.no;
+    request("http://www.omdbapi.com/?s=" + query + "&apikey=a56091af&page=" + page.toString(), function (error, response, body) {
+        let data = JSON.parse(body);
+        if(!error && response.statusCode === 200)
+            if(data.Response === "True"){
+                res.render("results", {movieData: data, query: query, no: parseInt(page)});
+            }
+            else{
+                res.send("notFound");
+            }
+    });
+})
+
+app.get("/full/:id", function (req, res) {
+    let id = req.params.id;
+    request("http://www.omdbapi.com/?i=" + id + "&apikey=a56091af&plot=full", function (error, response, body) {
+        let data = JSON.parse(body);
+        if(!error && response.statusCode === 200)
+            res.render("full", {data: data});
+    })
+});
+
+app.listen("3000", function () {
+    console.log("Movie App is Started");
+})
